5fc288108979a2fa8f8683259fcf456c9be1d56f
Sergio Orejarena <sergioorueda@gmail.com>
2021-04-28 14:43:28 -0500

    New form

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,7 @@
 
 from sys import argv
 
-
-def run():
+if __name__ == "__main__":
 
     if len(argv) - 1 == 0:
         print("{} argmuments.".format(len(argv) - 1))
@@ -13,6 +12,3 @@ def run():
 
         for i in range(1, len(argv)):
                 print("{}: {}".format(i, argv[i]))
-
-if __name__ == "__main__":
-    run()
