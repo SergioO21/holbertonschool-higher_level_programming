08acc7c861d4ed7c142fd9599d99bc8cef6ad50e
Sergio Orejarena <sergioorueda@gmail.com>
2021-04-28 12:15:32 -0500

    pep8 errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,17 +2,22 @@
 
 import sys
 
-argv = sys.argv
-addition = 0
 
-length = len(argv) - 1
+def run():
+    argv = sys.argv
+    addition = 0
 
-if length == 0:
+    length = len(argv) - 1
+
+    if length == 0:
+        print("{}".format(addition))
+
+    else:
+        for args in argv:
+            if args != "./3-infinite_add.py":
+                addition += int(args)
+
     print("{}".format(addition))
 
-else:
-    for args in argv:
-        if args != "./3-infinite_add.py":
-            addition += int(args)
-
-    print("{}".format(addition))
+if __name__ == "__main__":
+    run()
