db729985f2357c777a0410dbbcda44ec77f00cec
Sergio Orejarena <sergioorueda@gmail.com>
2021-04-28 11:36:40 -0500

    Infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+argv = sys.argv
+addition = 0
+
+length = len(argv) - 1
+
+if length == 0:
+    print("{}".format(addition))
+
+else:
+    for args in argv:
+        if args != "./3-infinite_add.py":
+            addition += int(args)
+
+    print("{}".format(addition))
