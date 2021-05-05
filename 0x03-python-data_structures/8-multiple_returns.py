942816cbaee40d4a55ff2ed06e1e8b8a499c114f
Sergio Orejarena <sergioorueda@gmail.com>
2021-05-05 13:10:44 -0500

    String Null conditional

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,11 @@
 
 def multiple_returns(sentence):
 
-    first_char = sentence[0]
+    first_char = None
+
+    if sentence:
+        first_char = sentence[0]
+
     len_sentence = len(sentence)
 
     return (len_sentence, first_char)
