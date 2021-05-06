7502d5d0ba3d2f7bd451f7156f56f3e86ebd6ded
Sergio Orejarena <sergioorueda@gmail.com>
2021-05-06 16:41:53 -0500

    Simple delete by key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def simple_delete(a_dictionary, key=""):
+
+    try:
+        del a_dictionary[key]
+
+    except KeyError:
+        return a_dictionary
+
+    return a_dictionary
