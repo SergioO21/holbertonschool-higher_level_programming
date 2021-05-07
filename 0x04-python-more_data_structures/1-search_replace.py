43118d80a37ac3212dff9324c0dcd7c3c5511f8c
Sergio Orejarena <sergioorueda@gmail.com>
2021-05-07 11:22:44 -0500

    New form

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,14 +3,4 @@
 
 def search_replace(my_list, search, replace):
 
-    l_list = len(my_list)
-    new_list = []
-
-    for i in range(l_list):
-        if my_list[i] == search:
-            new_list.append(replace)
-
-        else:
-            new_list.append(my_list[i])
-
-    return new_list
+    return list(replace if i == search else i for i in my_list)
