c22c71ef9668efb35f38ac39cc87da2a376ccb5e
Sergio Orejarena <sergioorueda@gmail.com>
2021-05-18 19:29:05 -0500

    Adding docstrings

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,7 +1,13 @@
+""" MagicClass Class """
 import math
 
 
 class MagicClass:
+
+    """ Defines a circle
+    Args:
+        radius (int, float) = Circle radius
+    """
 
     def __init__(self, radius=0):
 
@@ -13,7 +19,13 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
+
+        """ Calculates the area of the circle """
+
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
+
+        """ Calculates the circumference of the circle """
+
         return (2 * math.pi) * self.__radius
