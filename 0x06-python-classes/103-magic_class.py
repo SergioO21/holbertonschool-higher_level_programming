d926690aa72d62a902db444ec776e62dfc674dd4
Sergio Orejarena <sergioorueda@gmail.com>
2021-05-18 19:12:34 -0500

    Initialization

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python3
+
+import dis
+
+
 class MagicClass:
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
 
         if type(radius) is not int or type(radius) is not float:
             raise TypeError("radius must be a number")
@@ -12,3 +17,5 @@ class MagicClass:
 
     def circumference(self):
         return (2 * math.pi) * self.__radius
+
+dis.dis(MagicClass)
