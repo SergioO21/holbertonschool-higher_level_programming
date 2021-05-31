379b671448ab0c1584aef69809f8c20ce14eec4f
Sergio Orejarena <sergioorueda@gmail.com>
2021-05-31 18:03:59 -0500

    Task 5

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" JSON Decoder """
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    # Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+
+        my_str (...) = String to represent.
+    """
+
+    return json.loads(my_str)
