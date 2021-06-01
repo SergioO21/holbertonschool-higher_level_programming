3c7e08060d6bdcd36730a5c5e4de32264fe9c047
Sergio Orejarena <sergioorueda@gmail.com>
2021-05-31 19:26:39 -0500

    Task 8

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""  """
+
+from os import path
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def add_items():
+    """
+    # Adds all arguments to a Python list, and then save them to a file.
+    """
+
+    list_json = []
+
+    """ Check if file exists """
+    if path.isfile("add_item.json"):
+        list_json = load_from_json_file("add_item.json")
+
+    if len(argv) > 1:
+
+        for i in range(1, len(argv)):
+            list_json.append(argv[i])
+
+    """ Write the list with the new elements in the file """
+    save_to_json_file(list_json, "add_item.json")
+
+
+if __name__ == "__main__":
+    add_items()
