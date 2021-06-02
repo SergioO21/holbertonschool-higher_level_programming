02dee29323f59db03881fb0ae2d551e8335ac955
Sergio Orejarena <sergioorueda@gmail.com>
2021-06-02 18:59:30 -0500

    Integer validator

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -15,4 +15,5 @@ class Square(Rectangle):
     def __init__(self, size):
         super().__init__(size, size)
 
+        self.integer_validator("size", size)
         self.__size = size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,6 +15,7 @@ class Square(Rectangle):
     def __init__(self, size):
         super().__init__(size, size)
 
+        self.integer_validator("size", size)
         self.__size = size
 
     def __str__(self):
