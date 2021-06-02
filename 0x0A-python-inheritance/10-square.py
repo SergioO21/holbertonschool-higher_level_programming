272df9378a8bcebe5e0d40d77d8e6111231f6504
Sergio Orejarena <sergioorueda@gmail.com>
2021-06-02 16:03:27 -0500

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Square Class """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    # Defines a Square.
+
+    Args:
+        size (int) = Size of one side of the square.
+    """
+
+    def __init__(self, size):
+        super().__init__(size, size)
+
+        self.__size = size
