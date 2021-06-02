bde0e5e9c577586af21afae1e244e1d108edd7c9
Sergio Orejarena <sergioorueda@gmail.com>
2021-06-02 01:59:05 -0500

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" MyList Class """
+
+
+class MyList(list):
+    """
+    # Inherits from list.
+
+    - ``print_sorted(self)``: Prints the list, but sorted (ascending sort).
+    """
+    def print_sorted(self):
+        print(sorted(self))
