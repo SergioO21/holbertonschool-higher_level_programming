9d97c3b25529931c1d8b4327c46f594065ccb314
Sergio Orejarena <sergioorueda@gmail.com>
2021-07-13 17:47:30 -0500

    Auto unique ID's

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates the table force_name on your MySQL server.
+-- The database name will be passed as an argument of the mysql command.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256))
