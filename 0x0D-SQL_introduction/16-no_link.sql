899a57048b41a10f6665a32c8f9ea661e381df64
Sergio Orejarena <sergioorueda@gmail.com>
2021-07-12 20:31:55 -0500

    List by score only with names

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- Lists all records of the table second_table of the database hbtn_0c_0.
+--- Records should be listed by descending score.
+-- The database name will be passed as an argument of the mysql command.
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
