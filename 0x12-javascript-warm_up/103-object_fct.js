07aaa7387d9982f58703f8e34fe635f29fe5f0c1
Sergio Orejarena <sergioorueda@gmail.com>
2021-08-02 19:03:20 -0500

    Less code lines

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,11 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-
 myObject.incr = function () {
   myObject.value = myObject.value + 1;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
