582265922e1cfb1dd2fe774120c916aad6e808a7
Sergio Orejarena <sergioorueda@gamil.com>
2021-08-07 16:14:09 -0500

    Comments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa """
 
 from sys import argv
 import MySQLdb
 
 
 def main():
+    """ Main Function """
 
     db_conn = MySQLdb.connect(
             host="localhost",
