994581450c010ec4db49b30d53bee21d4c34e67c
Sergio Orejarena <sergioorueda@gamil.com>
2021-08-07 19:21:11 -0500

    All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Lists all cities from the database hbtn_0e_4_usa.
+"""
+
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """ Main Function """
+
+    db_conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8")
+
+    cur = db_conn.cursor()
+
+    cur.execute("""
+                SELECT cities.name
+                FROM cities
+                INNER JOIN states
+                ON cities.state_id = states.id
+                WHERE states.name LIKE BINARY %s
+                ORDER BY cities.id ASC""", (argv[4], ))
+
+    result = ""
+
+    for row in cur.fetchall():
+        result += row[0] + ", "
+
+    print(result[0:-2])
+
+    cur.close()
+    db_conn.close()
+
+
+if __name__ == "__main__":
+    main()
