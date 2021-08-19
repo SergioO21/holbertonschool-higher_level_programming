90f34c792456f55aaa6dafaba6117c67b82cd48e
Sergio Orejarena <sergioorueda@gmail.com>
2021-08-19 17:26:57 -0500

    Shebang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # Takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response.
 
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
+curl -sI "$1" | grep -i "Content-Length:" | cut -d " " -f 2
