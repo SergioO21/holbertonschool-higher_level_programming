0c67103547c4187b019e2c0dc1ff5a8b17c82ab0
Sergio Orejarena <sergioorueda@gmail.com>
2021-08-19 21:20:09 -0500

    New form

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sIo /dev/null --write-out '%{http_code}\n' "$1"
+curl -so /dev/null -w "%{http_code}\n" "$1"
