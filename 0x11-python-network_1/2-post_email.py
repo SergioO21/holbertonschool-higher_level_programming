3baf3eed8f85fdac87ceba69830a8773dcc00c19
Sergio Orejarena <sergioorueda@gmail.com>
2021-08-20 17:38:51 -0500

    POST an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Takes in a URL and an email, sends a POST request to the
+    passed URL with the email as a parameter, and displays
+    the body of the response (decoded in utf-8).
+"""
+
+
+def main():
+    """ Main Function """
+    import urllib.request
+    from sys import argv
+
+    url = argv[1]
+    data = urllib.parse.urlencode({"email": argv[2]})
+    data = data.encode("ascii")
+
+    post = urllib.request.urlopen(url, data)
+
+    with urllib.request.urlopen(post) as response:
+        print(response.read().decode("UTF-8"))
+
+
+if __name__ == "__main__":
+    main()
