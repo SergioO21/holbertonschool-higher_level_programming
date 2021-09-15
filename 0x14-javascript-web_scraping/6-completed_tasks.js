0aafa3159dafa3d73ea1224605f2f32d45be6422
Sergio Orejarena <sergioorueda@gmail.com>
2021-09-14 19:18:22 -0500

    Simplified

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,20 +7,16 @@ request(argv[0], function (err, res, body) {
   if (err) { console.log(err); }
 
   const data = JSON.parse(body);
-  let id = 1;
-  const task = {};
+  const taskCompleted = {};
 
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].userId === id) {
-      task[id] = 0;
-      id += 1;
+  for (const task of data) {
+    if (task.completed === true) {
+      if (task.userId in taskCompleted) {
+        taskCompleted[task.userId] += 1;
+      } else {
+        taskCompleted[task.userId] = 1;
+      }
     }
   }
-
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].completed === true) {
-      task[data[i].userId] += 1;
-    }
-  }
-  console.log(task);
+  console.log(taskCompleted);
 });
